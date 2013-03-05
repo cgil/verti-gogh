475847d27163fd7845d83108b248b4e84b6fc8cb
Alex Crichton <alex@alexcrichton.com>
2013-03-05 02:27:09 -0500

    New conversions that might work...

--- capture/lib.h
+++ capture/lib.h
@@ -12,9 +12,9 @@
 #define SIZE 10
 
 /* http://en.wikipedia.org/wiki/YCbCr */
-#define Y  ((int) (16 + 65.481 * R + 128.553 * G + 24.966 * B))
-#define Cb ((int) (128 + -37.797 * R - 74.203 * G + 112.0 * B))
-#define Cr ((int) (128 + 112.0 * R - 93.786 * G + 18.214 * B))
+#define Y  ((int) (0.299 * R + 0.587 * G + 0.114 * B))
+#define Cb ((int) (128 - 0.168736 * R - 0.331264 * G + 0.5 * B))
+#define Cr ((int) (128 + 0.5 * R - 0.418688 * G - 0.081312 * B))
 
 void reset();
 void process_rgb(void *buf, int size);
