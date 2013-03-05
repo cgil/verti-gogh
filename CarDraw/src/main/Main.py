8ab715e2bdea5fb3f151ab12bd4cbad2e1358335
Alex Crichton <alex@alexcrichton.com>
2013-03-05 02:14:15 -0500

    Add reading from stdin to python

--- CarDraw/src/main/Main.py
+++ CarDraw/src/main/Main.py
@@ -4,18 +4,20 @@ Created on Mar 4, 2013
 @author: carlosgil
 '''
 
+import sys
+
 from Classes import Car
 from Classes import VertCanvas
-from Tkinter import Tk, BOTH
+from Tkinter import Tk, BOTH, tkinter
 from random import randint
         
 root = Tk()
-width = 800
-height = 600
+width = 320
+height = 240
 root.geometry(str(width)+"x"+str(height))
 canvas = VertCanvas.VertCanvas(root)
 cars = []
-for i in range(2) : 
+for i in range(1) :
     cars.append(Car.Car(i,randint(1,width),randint(1,height), canvas))
 
 
@@ -26,7 +28,18 @@ def draw():
     canvas.pack(fill=BOTH, expand=1)
     root.after(1000, draw)
 
-root.after(1000, draw)
+def readappend(fh, _):
+    mystr = sys.stdin.readline()
+    print mystr
+    idx, row, col = mystr.split(' ')
+    cars[int(idx)].addPoint(int(row), int(col))
+    cars[int(idx)].display()
+    canvas.pack(fill=BOTH, expand=1)
+
+if len(sys.argv) > 1 && sys.argv[1] == 'rand'
+  root.after(1000, draw)
+else
+  root.tk.createfilehandler(sys.stdin, tkinter.READABLE,
+                            readappend)
+
 root.mainloop()  
-
-
