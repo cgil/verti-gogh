e479f6bcb62b0cd4fda7d8748e8027e76667497b
Alex Crichton <alex@alexcrichton.com>
2013-04-24 13:04:43 -0400

    Actually correct code...

--- irtoy/irtoy.py
+++ irtoy/irtoy.py
@@ -27,126 +27,50 @@ __email__ = 'crleblanc@gmail.com'
 class IrToy(object):
 
     def __init__(self, serialDevice):
-        '''Create a new IrToy instance using the serial device for the USB IR Toy'''
         self.toy = serialDevice
-
-        self.sleepTime = 0.05
-        self.handshake = None
-        self.byteCount = None
-        self.complete = None
-
-        # always use sampling mode
+        # ir toy must be in sampling mode, even for transmissions to work
         self._setSamplingMode()
-
-        self.transmitMode = False
-
-    def _sleep(self):
-        time.sleep(self.sleepTime)
 
     def _setSamplingMode(self):
         '''set the IR Toy to use sampling mode, which we use exclusively'''
         self.reset()
-
         self.toy.write(b'S')
+        protocolVersion = self.toy.read(3)
+        if protocolVersion != 'S01':
+          raise IOError('Expected protocol "S01", got %s' % protocolVersion)
 
-        self._sleep()
-
-        self.protocolVersion = self.toy.read(3)
-
-    def _writeList(self, code, check_handshake=False):
-        '''write a list like object of integer values'''
-
-        self._sleep()
-
+    def _acknowledge(self, code):
         byteCode = bytearray(code)
-        bytesWritten = 0
+        for idx in range(0, len(code), 62):
+            self.toy.write(byteCode[idx:idx+62])
+            if ord(self.toy.read(1)) != 0x3e:
+              raise IOError('invalid acknowledgement received')
 
-        # 31 * 2 bytes = max of 62 bytes in the buffer.  31 hangs so using 32, strange.
-        maxWriteSize = 32
-        for idx in range(0, len(code), maxWriteSize):
-            segmentWritten = self.toy.write(byteCode[idx:idx+maxWriteSize])
-            bytesWritten += segmentWritten
-
-            if check_handshake:
-                self.handshake = ord(self.toy.read(1))
-
-        if bytesWritten != len(code):
-            raise IOError('incorrect number of bytes written to serial device, expected %d' % len(code))
-
-    def _getTransmitReport(self):
-        '''get the byteCount and completion status from the IR Toy'''
-
-        hexBytes = binascii.b2a_hex(self.toy.read(3)[1:])
-        self.byteCount = int(hexBytes, 16)
-        self.complete = self.toy.read(1)
-
-    def _setTransmit(self):
-        self._sleep()
-        self._writeList([0x26]) #Enable transmit handshake
-        self._writeList([0x25]) #Enable transmit notify on complete
-        self._writeList([0x24]) #Enable transmit byte count report
-        self._writeList([0x03], check_handshake=True) #Expect to receive packets to transmit
-
-        self.transmitMode = True
-
-    def receive(self):
-        '''Read a signal from the toy, returns a list of IR Codes converted from hex to ints.  See 
-        http://dangerousprototypes.com/docs/USB_IR_Toy:_Sampling_mode for more information on the
-        sample format.  Reading starts when an IR signal is received and stops after 1.7 seconds of 
-        inactivity'''
-
-        self._sleep()
-
-        # reset and put back in receive mode in case it was in transmit mode or there was junk in the buffer
-        self._setSamplingMode()
-
-        bytesToRead=1
-        readCount=0
-        irCode = []
-
-        while(True):
-            readVal = self.toy.read(bytesToRead)
-            hexVal = binascii.b2a_hex(readVal)
-            intVal = int(hexVal, 16)
-
-            irCode.append(intVal)
-
-            if readCount >= 2 and intVal == 255 and irCode[-2] == 255:
-                break
-
-            readCount += 1
-
-        self._sleep()
-
-        return irCode
 
     def reset(self):
-        '''Reset the IR Toy to sampling mode and clear the Toy's 62 byte buffer'''
-
-        self._sleep()
-        self._writeList([0x00]*5)
+        self.toy.write(bytearray([0x00] * 5))
 
     def transmit(self, code):
-        '''switch to transmit mode and write the list (or list-like) set of ints to the toy for transmission,
-        Must be an even number of elements.  The list created by read() can be used for transmitting.  If the
-        last two elements of the list are not 0xff these will be appended.'''
-
-        if len(code) < 2:
-            raise ValueError("Length of code argument must be greater than or equal to two")
-
         if len(code) % 2 != 0:
             raise ValueError("Length of code argument must be an even number")
 
-        # ensure the last two codes are always 0xff (255) to tell the IR Toy it's the end of the signal
+        # ensure the last two codes are always 0xff (255) to tell the IR Toy
+        # it's the end of the signal
         if code[-2:] != [0xff, 0xff]:
             code.extend([0xff, 0xff])
 
-        self._sleep()
-        self._setTransmit()
-        self._writeList(code, check_handshake=True)
-        self._sleep()
-        self._getTransmitReport()
-        
-        # experimentation shows that a reset is needed to avoid dropping the serial connection on Linux
-        self.reset()
-        self._sleep()
+        # Enable transmit handshake, notify on complete, byte count report, and
+        # then sent the actual transmit command
+        self._acknowledge([0x26, 0x25, 0x24, 0x03])
+
+        # Send the actual data (chunked inside)
+        self._acknowledge(code)
+
+        # Acknowledge the data was actually sent, and all of it was sent.
+        if self.toy.read(1) != 't':
+          raise IOError('expected a "t" acknowledgement')
+        hexBytes = binascii.b2a_hex(self.toy.read(2))
+        if self.toy.read(1) != 'C':
+          raise IOError('expected a completion "C"')
+        if int(hexBytes, 16) != len(code):
+          raise IOError('short write')
