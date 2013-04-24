0962bc7cb1d9a44047a48579f85df9b6b5304f98
Alex Crichton <alex@alexcrichton.com>
2013-04-24 13:31:38 -0400

    Add some code to control shitty chinese cars

--- irtoy/control.py
+++ irtoy/control.py
@@ -2,91 +2,93 @@ import serial
 import time
 from irtoy import IrToy
 
+# The pre command locks all non-locked cars onto this channel. The pre comand
+# will also cease the car's movement. Otherwise all commands last for XXX
+# seconds on the car before the car automatically stops that command.
+class ShittyChineseCar:
+  def __init__(self, toy, channel):
+    self.toy = toy
+    self.channel = channel
 
-print "opening"
+  def pre(self):
+    self._send('011011')
+
+  def forward(self):
+    self._send('000011')
+
+  def backward(self):
+    self._send('110011')
+
+  def left(self):
+    self._send('011110')
+
+  def right(self):
+    self._send('011000')
+
+  def forwardright(self):
+    self._send('000000')
+
+  def forwardleft(self):
+    self._send('000110')
+
+  def backwardright(self):
+    self._send('110000')
+
+  def backwardleft(self):
+    self._send('110110')
+
+  def _send(self, cmd):
+    # Bits are each 13 bits: 1CC0XXXXXX0LR (L/R are trim, just be 0)
+    # The commands sent to the irtoy have 4 bytes for each bit, where the first
+    # pair of bytes is a 16-bit integer representing a high period of activity
+    # and the second pair is a low period. All of the low periods are the same,
+    # but the high periods vary
+
+    b = [0, 140, 0, 47]             # 1
+    if self.channel == 0:           # CC
+      b.extend([0, 47, 0, 47])
+      b.extend([0, 47, 0, 47])
+    elif self.channel == 1:
+      b.extend([0, 47, 0, 47])
+      b.extend([0, 94, 0, 47])
+    elif self.channel == 2:
+      b.extend([0, 94, 0, 47])
+      b.extend([0, 47, 0, 47])
+    else:
+      b.extend([0, 94, 0, 47])
+      b.extend([0, 94, 0, 47])
+    b.extend([0, 47, 0, 47])        # 0
+
+    assert len(cmd) == 6
+    for c in cmd:                   # XXXXXX
+      if c == '0':
+        b.extend([0, 47, 0, 47])
+      else:
+        b.extend([0, 94, 0, 47])
+
+    b.extend([0, 47, 0, 47])        # 0
+    b.extend([0, 47, 0, 47])        # L
+    b.extend([0, 47, 0, 47])        # R
+    assert len(b) == 13 * 4
+    self.toy.transmit(b)
+
+
 with serial.Serial('/dev/ttyACM0') as serialDevice:
-  print "opened"
   t = IrToy(serialDevice)
-  print "created"
+  c = ShittyChineseCar(t, 0)
 
-  # print 'receiving'
-  # code = t.receive()
-  # print code
-  print "sending\n\n\n"
-  # t.transmit([0, 140, 0, 47,
-  #             0, 47, 0, 47,
-  #             0, 94, 0, 47,
-  #             0, 47, 0, 47,
-  #             0, 47, 0, 47,
-  #             0, 94, 0, 47,
-  #             0, 94, 0, 47,
-  #             0, 47, 0, 47,
-  #             0, 47, 0, 47,
-  #             0, 47, 0, 47,
-  #             0, 47, 0, 47,
-  #             0, 47, 0, 47,
-  #             0, 47, 0, 47])
-  # '101000001100# 0'
-  # print t.receive()
+  print 'forward'
+  c.pre()
+  c.forward()
+  time.sleep(0.2)
 
-  pre  =     [0, 140, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 94, 0, 47,
-              0, 94, 0, 47,
-              0, 47, 0, 47,
-              0, 94, 0, 47,
-              0, 94, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 8, 47,
-              ]
-  foo  =     [0, 140, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 94, 0, 47,
-              0, 94, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 94, 0, 47,
-              0, 94, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 200, 47,
-              ]
+  print 'left'
+  c.pre()
+  c.left()
+  time.sleep(0.1)
 
-  # foo.extend(foo);
-  # foo.extend(foo);
-  # foo.extend(foo);
-  # foo.extend(foo);
-  # foo.extend(foo);
-  # foo.extend(foo);
-  # foo.extend(foo);
-  # foo.extend(foo);
-  # foo.extend(foo);
-  # foo.extend(foo);
-
-  bar = []
-  bar.extend(pre)
-  bar.extend(foo)
-  # bar.extend(pre)
-  bar.extend([0, 140, 0, 47,
-              0, 94, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 94, 0, 47,
-              0, 94, 0, 47,
-              0, 47, 0, 47,
-              0, 94, 0, 47,
-              0, 94, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 0, 47,
-              0, 47, 8, 47,
-              ])
-
-  t.transmit(bar)
+  print 'forward'
+  c.pre()
+  c.forward()
+  time.sleep(0.1)
 
